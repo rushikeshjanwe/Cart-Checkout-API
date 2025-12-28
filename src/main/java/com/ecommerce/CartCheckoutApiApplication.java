ca8eb580f759d8ddd2014e94fef1655cbb7cb41b
Rushikesh Janwe <90002061+rushikeshjanwe@users.noreply.github.com>
2025-12-28 17:30:28 +0530

    Update CartCheckoutApiApplication.java

--- src/main/java/com/ecommerce/CartCheckoutApiApplication.java
+++ src/main/java/com/ecommerce/CartCheckoutApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CartCheckoutApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CartCheckoutApiApplication.class, args);
     }
